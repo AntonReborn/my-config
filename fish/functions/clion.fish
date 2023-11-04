f9eef6918e296a3335cd2f5a8551257652cdb763
Anton Krasnokutskiy <antonreborn777@gmail.com>
2023-11-03 21:42:22 -0700

    Added fish.

---
+++ fish/functions/clion.fish
@@ -0,0 +1,3 @@
+function clion --wraps=/home/anton/Software/clion-2023.2.2/bin/clion.sh --description 'alias clion /home/anton/Software/clion-2023.2.2/bin/clion.sh'
+  /home/anton/Software/clion-2023.2.2/bin/clion.sh $argv; 
+end
